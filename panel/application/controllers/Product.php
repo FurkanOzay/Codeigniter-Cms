3c9cfab431048ff6dbdeee3a5f3fc674c96f84ad
Furkan Özay <furkanozay0@gmail.com>
2022-09-04 00:24:30 +0300

    ürün listesi veri tabanından çekildi.product model kodlandı

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -8,14 +8,29 @@ class Product extends  CI_Controller {
     {
         parent::__construct();
         $this->viewFolder = "product_v";
+
+        $this->load->model("product_model");
+
     }
 
     //index metodu altına sadece listeleme yapmak istediğimiz kodları kontrol ediyoruz.
     public function index(){
 
         $viewData = new stdClass();
+
+        /* Tablodan Verilerin Getirilmesi */
+        $items = $this->product_model->get_all();
+        /* 1 - Veritabanında ki  verileri getir */
+        /* 2 - items isimli değişkene yazdır */
+
+        /* print_r($items); */
+        /* items'ı ekrana yazdır. */
+        /* View'e gönderilecek değişkenlerin set edilmesi.
+        Veritabanından gelen verileri viewde göstermek istiyorsak bu item'ı view'e göndermemiz gerekiyor. */
         $viewData->viewFolder = $this->viewFolder;
         $viewData->subViewFolder = "list";
+        $viewData->items = $items;
+
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
 
     }
--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -10,9 +10,12 @@
     </div><!-- END column -->
     <div class="col-md-12">
         <div class="widget p-lg">
+            <!-- empty ile değişkenin boş olup olmaadığını kontrol ediyoruz -->
+            <?php if(empty($items)) { ?>
             <div class="alert alert-info text-center">
                 <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için <a href="#">tıklayınız.</a></p>
             </div>
+            <?php } else { ?>
             <!-- .table-striped: 1'er sütun aralıklarla renklendirme yapar -->
             <!-- .table-hover: Her sütunun üstüne gelince renk değiştirmesini sağlar -->
             <table class="table table-hover table-striped">
@@ -25,33 +28,38 @@
                 <th class="text-center">İşlem</th>
                 </thead>
                 <tbody class="text-center">
+                <?php foreach ($items as $item) { ?>
                 <tr>
-                    <td>#1</td>
-                    <td>monitor-askisi</td>
-                    <td>Monitör</td>
-                    <td>360 derece kullanılabilen monitör askısıdır.</td>
+                    <td>#<?php echo $item->id; ?></td>
+                    <td><?php echo $item->url; ?></td>
+                    <td><?php echo $item->title; ?></td>
+                    <td><?php echo $item->description; ?></td>
                     <td>
                         <input
                                 type="checkbox"
                                 data-switchery
                                 data-color="#10c469"
                                 data-size="small"
-                                checked
+                                <!-- isActive 1 ise toggle seçili 0 ise seçilmemiş görünmesi için koşul oluşturuyoruz. -->
+                                <?php echo ($item->isActive) ? "checked" : ""; ?>
                         >
                     </td>
                     <td>
-                        <a href="#" class="btn btn-xs btn-danger btn-outline">
+                        <a href="#" class="btn btn-xs btn-inverse btn-outline">
                             <i class="fa fa-trash"></i>
                             Sil
                         </a>
-                        <a href="#" class="btn btn-xs btn-warning btn-outline rounded">
+                        <a href="#" class="btn btn-xs btn-inverse btn-outline rounded">
                             <i class="fa fa-pencil-square-o"></i>
                             Düzenle
                         </a>
                     </td>
                 </tr>
+                <?php } ?>
                 </tbody>
             </table>
+
+            <?php } ?>
         </div><!-- .widget -->
     </div><!-- END column -->
 </div>
